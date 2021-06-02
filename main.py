1cf2bffc0b15015e419059a1fb3e1a35916ff5b2
Kamuzinzi <50994146+Kamuzinzi@users.noreply.github.com>
2021-06-02 22:25:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, Request, Response
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+import httpx
+import os
+
+
+app = FastAPI()
+
+
+
+# Request Models.
+class WebhookRequestData(BaseModel):
+    object: str = ""
+    # entry: List = []
+
+
+# Helpers.
+async def send_message(
+    page_access_token: str,
+    recipient_id: str,
+    message_text: str,
+    message_type: str = "UPDATE",
+):
+    """
+    Send message to specific user(By recipient ID) from specific page(By
+    access token).
+    Arguments:
+        page_access_token: (string) Target page access token.
+        recipient_id: (string) The ID of the user that the message is
+         addressed to.
+        message_text: (string) The content of the message.
+        message_type: (string) The type of the target message.
+         RESPONSE, UPDATE or MESSAGE_TAG - the accurate description -
+         https://developers.facebook.com/docs/messenger-platform/send-messages/#messaging_types
+    """
+    r = httpx.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params={"access_token": page_access_token},
+        headers={"Content-Type": "application/json"},
+        json={
+            "recipient": {"id": recipient_id},
+            "message": {"text": message_text},
+            "messaging_type": message_type,
+        },
+    )
+    r.raise_for_status()
+
+
+
+
+verify= "<LetsLearnFrenchNow>"
+secret= "a40c30b2bc6baeab10759995f20f99c2"
+page_access_token= "EAAGqxHBIeVUBAEKrspc2MFtCkun5MHUayZBUvUfv4AUhhu6MC7MyZB5cPvf0RGHFOOzd8l5xBZBoV2EPVV6obquSSEaekz3ERaoHpqBFEzZBrWLrlDNnf665hECKJ8X377sd5d64J7y1NuiLRWDzlo87hfox6SJ9Qxsjht2GtXLyUBL1v8qt"
+
+# Endpoints.
+@app.router.get("/api/webhook")
+async def verify(request: Request):
+    """
+    On webook verification VERIFY_TOKEN has to match the token at the
+    configuration and send back "hub.challenge" as success.
+    """
+    if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get(
+        "hub.challenge"
+    ):
+        if (
+            not request.query_params.get("hub.verify_token")
+            == os.environ["VERIFY_TOKEN"]
+        ):
+            return Response(content="Verification token mismatch", status_code=403)
+        return Response(content=request.query_params["hub.challenge"])
+
+    return Response(content="Required arguments haven't passed.", status_code=400)
+
+
+@app.router.post("/api/webhook")
+async def webhook(data: WebhookRequestData):
+    """
+    Messages handler.
+    """
+    if data.object == "page":
+        for entry in data.entry:
+            messaging_events = [
+                event for event in entry.get("messaging", []) if event.get("message")
+            ]
+            for event in messaging_events:
+                message = event.get("message")
+                sender_id = event["sender"]["id"]
+
+                await send_message(page_access_token=os.environ["PAGE_ACCESS_TOKEN"],
+                                   recipient_id=sender_id,
+                                   message_text=f"echo: {message['text']}")
+
+    return Response(content="ok")
+
+
+# Debug.
+def main():
+    if "VERIFY_TOKEN" in os.environ:
+        print("your verify token is: ", os.environ["VERIFY_TOKEN"])
+
+    uvicorn.run(app=app)
+
+
+if __name__ == "__main__":
+    main()
